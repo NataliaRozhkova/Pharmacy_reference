151e95b85bbed6c1b0ec38bac29cd4d848131804
Natasha <nat.n.rozhkova@gmail.com>
2021-01-19 13:15:59 +0500

    MedicineParser fix and go to debaging

--- src/main/java/pharmacy/reference/data/entity/parser/MedicineParser.java
+++ src/main/java/pharmacy/reference/data/entity/parser/MedicineParser.java
@@ -8,22 +8,22 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.tika.parser.txt.CharsetDetector;
+import org.apache.tika.parser.txt.CharsetMatch;
 import pharmacy.reference.data.entity.Medicine;
 import pharmacy.reference.data.entity.Pharmacy;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
 public class MedicineParser {
 
-    private int columnMedicineNameNumber = -1;
-    private int columnMedicinePriceNumber = -1;
-    private int columnMedicineQuantityNumber = -1;
-    private int columnMedicineManufactureNumber = -1;
+    private final HashMap<String, Integer> columnNumberNames = new HashMap<>();
 
-    private Pharmacy pharmacy;
+    private final Pharmacy pharmacy;
 
     public MedicineParser(Pharmacy pharmacy) {
         this.pharmacy = pharmacy;
@@ -35,72 +35,84 @@ public class MedicineParser {
         try {
             inputStream = new FileInputStream(file);
             if (FilenameUtils.getExtension(file.getAbsolutePath()).equalsIgnoreCase("xls")) {
-                return parseExcel(file, new HSSFWorkbook(inputStream));
+                return parseExcel(new HSSFWorkbook(inputStream));
             } else if (FilenameUtils.getExtension(file.getAbsolutePath()).equalsIgnoreCase("xlsx")) {
-                return parseExcel(file, new XSSFWorkbook(inputStream));
+                return parseExcel(new XSSFWorkbook(inputStream));
             } else if (FilenameUtils.getExtension(file.getAbsolutePath()).equalsIgnoreCase("csv")) {
-                return parseCSV(file);
+                return parseCSV(file, inputStream);
             }
-        } catch (FileNotFoundException fileNotFoundException) {
-            fileNotFoundException.printStackTrace();
         } catch (IOException ioException) {
             ioException.printStackTrace();
+
         } finally {
             try {
-                inputStream.close();
-            } catch (IOException ioException) {
-                ioException.printStackTrace();
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
         return null;
     }
 
-    private List<Medicine> parseExcel(File file, Workbook workBook) {
-        InputStream inputStream = null;
+    private List<Medicine> parseExcel(Workbook workBook) {
         List<Medicine> medicines = new ArrayList<>();
-
         Sheet sheet = workBook.getSheetAt(0);
         Iterator<Row> it = sheet.iterator();
-        setColumnTypeNumberExcel(it.next());
+        do {
+            setColumnTypeNumberExcel(it.next());
+        } while (columnNumberNames.get(TableHeadVariants.NAME) == -1 && columnNumberNames.get(TableHeadVariants.PRICE) == -1 && it.hasNext());
 
         while (it.hasNext()) {
             Medicine medicine = parseMedicineFromRowExel(it.next());
-            if (!medicine.getName().equals("")) {
+            if (medicine != null && !medicine.getName().equals("")) {
+                medicine.setPharmacy(pharmacy);
                 medicines.add(medicine);
             }
         }
         return medicines;
     }
 
-    private List<Medicine> parseCSV(File file) {
+    private List<Medicine> parseCSV(File file, InputStream stream) {
         InputStreamReader isr = null;
         List<Medicine> medicines = new ArrayList<>();
         try {
-            isr = new InputStreamReader(new FileInputStream(file), "windows-1251");
-            CSVReader cvsr = new CSVReader(isr, ';');
-            List<String[]> allRows = cvsr.readAll();
+            isr = new InputStreamReader(stream, getFileEncodingType(file));
+            CSVReader reader = new CSVReader(isr, ';', ':', '\n');
+            List<String[]> allRows = reader.readAll();
             setColumnTypeNumberCSV(allRows.get(0));
 
             for (int row = 1; row < allRows.size(); row++) {
                 Medicine medicine = parseMedicineFromRowCSV(allRows.get(row));
                 if (!medicine.getName().equals("")) {
+                    medicine.setPharmacy(pharmacy);
                     medicines.add(medicine);
                 }
             }
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            try {
-                isr.close();
-            } catch (IOException e) {
-                e.printStackTrace();
+            if (isr != null) {
+
+                try {
+                    isr.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
             }
         }
         return medicines;
+    }
+
+    private String getFileEncodingType(File file) throws IOException {
+        CharsetDetector charsetDetector = new CharsetDetector();
+        FileInputStream inputStream = new FileInputStream(file);
+        charsetDetector.setText(inputStream.readAllBytes());
+        CharsetMatch match = charsetDetector.detect();
+        inputStream.close();
+        return match.getName();
     }
 
     private Medicine parseMedicineFromRowExel(Row row) {
@@ -109,21 +121,25 @@ public class MedicineParser {
         Medicine medicine = new Medicine();
         while (cells.hasNext()) {
             Cell cell = cells.next();
+            try {
+                if (column == columnNumberNames.get(TableHeadVariants.NAME)) {
+                    medicine.setName(cell.getStringCellValue());
+                }
+                if (column == columnNumberNames.get(TableHeadVariants.PRICE)) {
+                    medicine.setPrice((float) cell.getNumericCellValue());
+                }
+                if (column == columnNumberNames.get(TableHeadVariants.QUANTITY)) {
+                    medicine.setQuantity((int) cell.getNumericCellValue());
+                }
+                if (column == columnNumberNames.get(TableHeadVariants.MANUFACTURE)) {
+                    medicine.setManufacturer(cell.getStringCellValue());
+                }
+                medicine.setPharmacy(pharmacy);
+            } catch (RuntimeException e) {
+                medicine = null;
+                break;
 
-            if (column == columnMedicineNameNumber) {
-                medicine.setName(cell.getStringCellValue());
             }
-            if (column == columnMedicinePriceNumber) {
-                medicine.setPrice((float) cell.getNumericCellValue());
-            }
-            if (column == columnMedicineQuantityNumber) {
-                medicine.setQuantity((int) cell.getNumericCellValue());
-
-            }
-            if (column == columnMedicineManufactureNumber) {
-                medicine.setManufacturer(cell.getStringCellValue());
-            }
-            medicine.setPharmacy(pharmacy);
             column++;
         }
         return medicine;
@@ -132,19 +148,27 @@ public class MedicineParser {
     private Medicine parseMedicineFromRowCSV(String[] row) {
         Medicine medicine = new Medicine();
         for (int column = 0; column < row.length; column++) {
-            if (column == columnMedicineNameNumber) {
-                medicine.setName(row[column]);
+            try {
+                if (column == columnNumberNames.get(TableHeadVariants.NAME)) {
+                    medicine.setName(row[column]);
+                }
+                if (column == columnNumberNames.get(TableHeadVariants.PRICE)) {
+                    try {
+                        medicine.setPrice(Float.parseFloat(row[column]));
+                    } catch (NumberFormatException e) {
+                        medicine.setPrice(Float.parseFloat(row[column].replace(',', '.')));
+                    }
+                }
+                if (column == columnNumberNames.get(TableHeadVariants.QUANTITY)) {
+                    medicine.setQuantity(Integer.parseInt(row[column]));
+                }
+                if (column == columnNumberNames.get(TableHeadVariants.MANUFACTURE)) {
+                    medicine.setManufacturer(row[column]);
+                }
+            } catch (RuntimeException e) {
+                medicine = null;
+                break;
             }
-            if (column == columnMedicinePriceNumber) {
-                medicine.setPrice(Float.parseFloat(row[column]));
-            }
-            if (column == columnMedicineQuantityNumber) {
-                medicine.setQuantity(Integer.parseInt(row[column]));
-            }
-            if (column == columnMedicineManufactureNumber) {
-                medicine.setManufacturer(row[column]);
-            }
-            medicine.setPharmacy(pharmacy);
         }
         return medicine;
     }
@@ -155,43 +179,17 @@ public class MedicineParser {
         int columnNumber = 0;
         while (cells.hasNext()) {
             Cell cell = cells.next();
-            setTableHeadNumbers(cell.getStringCellValue(), columnNumber++);
+            columnNumberNames.put(TableHeadVariants.tableHeadParse(cell.getStringCellValue()), columnNumber++);
         }
     }
 
     private void setColumnTypeNumberCSV(String[] row) {
         int columnNumber = 0;
         for (String cell : row) {
-            setTableHeadNumbers(cell, columnNumber++);
+            columnNumberNames.put(TableHeadVariants.tableHeadParse(cell), columnNumber++);
+
         }
     }
 
-    private void setTableHeadNumbers(String cellValue, int columnNumber) {
-        cellValue = cellValue.trim().toLowerCase().replaceAll("\\s+", "");
-        switch (cellValue) {
-            case "наименование":
-            case "товар":
-            case "наименованиетовара":
-            case "названиетовара":
-            case "название":
-            case "номенклатура":
-                columnMedicineNameNumber = columnNumber;
-                break;
-            case "производитель":
-            case "фирма-производитель":
-                columnMedicineManufactureNumber = columnNumber;
-                break;
-            case "количество":
-            case "кол-во":
-            case "количество товара":
-                columnMedicineQuantityNumber = columnNumber;
-                break;
-            case "цена":
-            case "цена,руб.":
-            case "средниеценыценарозн":
-            case "средняяцена":
-                columnMedicinePriceNumber = columnNumber;
-                break;
-        }
-    }
+
 }
