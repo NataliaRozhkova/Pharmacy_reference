029bbf99b2f04cf9d9e54922a4f57299d425ce8b
Natasha <nat.n.rozhkova@gmail.com>
2021-04-02 11:46:42 +0500

    StatisticController scheduled method customization

--- src/main/java/pharmacy/reference/spring_server/controllers/StatisticController.java
+++ src/main/java/pharmacy/reference/spring_server/controllers/StatisticController.java
@@ -131,9 +131,9 @@ public class StatisticController {
     //* "0 0 8-10 * * *" = 8, 9 and 10 o'clock of every day.
     //* "0 0/30 8-10 * * *" = 8:00, 8:30, 9:00, 9:30 and 10 o'clock every day.
     //* "0 0 9-17 * * MON-FRI" = on the hour nine-to-five weekdays
-
     //* "0 0 0 25 12 ?" = every Christmas Day at midnight
-    @Scheduled(cron = "0 00 7 * * *")
+
+    @Scheduled(cron = "0 0 7 1 * *")
     public void autoCreateStatAllFiles() throws IOException, ParseException {
         int month = Calendar.getInstance().get(Calendar.MONTH);
         int year = Calendar.getInstance().get(Calendar.YEAR);
