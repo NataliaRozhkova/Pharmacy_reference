9db9e289805658e068488438c63447e9cdc3efef
Natasha <nat.n.rozhkova@gmail.com>
2021-04-02 11:36:29 +0500

    WebSecurityConfig.java create new users

--- src/main/java/pharmacy/reference/spring_server/web/WebSecurityConfig.java
+++ src/main/java/pharmacy/reference/spring_server/web/WebSecurityConfig.java
@@ -6,35 +6,33 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.AuthenticationEntryPoint;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private YAMLConfig config;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers( "/file").hasRole("ADMIN")
-                .antMatchers( "/file/**").hasRole("ADMIN")
-                .antMatchers( "/pharmacy/parse").hasRole("ADMIN")
+                .antMatchers("/file").hasRole("ADMIN")
+                .antMatchers("/file/**").hasRole("ADMIN")
+                .antMatchers("/pharmacy/parse").hasRole("ADMIN")
+                .antMatchers("/auxiliary").hasRole("ADMIN")
+                .antMatchers("/email").hasRole("ADMIN")
+
                 .antMatchers("/webjars/**").permitAll()
                 .antMatchers("/", "/home").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login")
-                .defaultSuccessUrl("/",true)
+                .defaultSuccessUrl("/", true)
                 .permitAll()
                 .and()
                 .logout()
@@ -51,14 +49,32 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        auth
-                .inMemoryAuthentication()
-                .passwordEncoder(passwordEncoder)
-                .withUser("user").password(passwordEncoder.encode("user")).roles("USER")
-                .and()
-                .withUser("admin").password(passwordEncoder.encode("123")).roles("USER", "ADMIN");
 
+        auth.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder)
+                .withUser("user1")
+                .password(passwordEncoder.encode(config.getUsers().get("user1").getPassword()))
+                .roles("USER")
+                .and()
+                .withUser("user2")
+                .password(passwordEncoder.encode(config.getUsers().get("user2").getPassword()))
+                .roles("USER")
+                .and()
+                .withUser("user3")
+                .password(passwordEncoder.encode(config.getUsers().get("user3").getPassword()))
+                .roles("USER")
+                .and()
+                .withUser("user4")
+                .password(passwordEncoder.encode(config.getUsers().get("user4").getPassword()))
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password(passwordEncoder.encode(config.getUsers().get("admin").getPassword()))
+                .roles("USER", "ADMIN");
     }
 
-
+    @Autowired
+    public void setConfig(YAMLConfig config) {
+        this.config = config;
+    }
 }
